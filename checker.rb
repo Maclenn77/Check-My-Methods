a8f8e8b6745fe95aaa699518caab40ad53675db6
Maclenn77 <maclenn77@gmail.com>
2020-04-22 20:18:13 -0500

    Changed rules to functios

--- checker.rb
+++ checker.rb
@@ -1,8 +1,6 @@
 #!/usr/bin/ruby
 require_relative 'My_Methods'
 
-bar = "\n=====================\n"
-
 # Your methods should acept Range class
 range = (3..8)
 
@@ -16,6 +14,7 @@ boolean_array = [nil, false, true]
 str = 'strings'
 
 # Proc with one variable
+
 block_1 = proc { |n| print n.to_s + " " }
 
 #Block with two variables
@@ -23,79 +22,95 @@ block_2 = proc { |n, m| n * m }
 
 # Test my_each
 
-print bar + 'Each - No arguments' + bar + "\n"
+def my_title(title)
+  bar = "\n=====================\n"
+  print bar + title + bar + "\n"
+end
 
-puts 'My method: ' + range.my_each.to_s
-puts "Ruby's method: " + range.each.to_s
+def check_each(range)
+  my_title('Each - No arguments')
 
-print bar + 'Each - Proc' + bar + "\n"
+  puts 'My method: ' + range.my_each.to_s
+  puts "Ruby's method: " + range.each.to_s
 
-puts 'My method: ' + range.my_each(&block_1).to_s
-puts "Ruby's method: " + range.my_each(&block_1).to_s
+  my_title('Each - Proc')
 
-print bar + 'Each - Block Given' + bar + "\n"
+  puts 'My method: ' + range.my_each(&block_1).to_s
+  puts "Ruby's method: " + range.my_each(&block_1).to_s
 
-puts 'My method: ' + (range.my_each { |n| print n.to_s + " " }).to_s
-puts "Ruby's method: " + (range.each { |n| print n.to_s + " " }).to_s
+  my_title('Each - Block Given')
+
+  puts 'My method: ' + (range.my_each { |n| print n.to_s + ' ' }).to_s
+  puts "Ruby's method: " + (range.each { |n| print n.to_s + ' ' }).to_s
+end
 
 # Test my_each with each_with_index
 
-print bar + 'Each with index - No arguments' + bar + "\n"
+def check_each_with_index
+  print bar + 'Each with index - No arguments' + bar + "\n"
 
-puts 'My method: ' + range.my_each_with_index.to_s
-puts "Ruby's method: " + range.each_with_index.to_s
+  puts 'My method: ' + range.my_each_with_index.to_s
+  puts "Ruby's method: " + range.each_with_index.to_s
 
-print bar + 'Each with index - Proc' + bar + "\n"
+  print bar + 'Each with index - Proc' + bar + "\n"
 
-puts 'My method: ' + range.my_each_with_index(&block_2).to_s
-puts "Ruby's method: " + range.each_with_index(&block_2).to_s
+  puts 'My method: ' + range.my_each_with_index(&block_2).to_s
+  puts "Ruby's method: " + range.each_with_index(&block_2).to_s
+end
 
 # Test my_all
 
-print bar + 'All? - No arguments' + bar + "\n"
+def check_my_all
+  print bar + 'All? - No arguments' + bar + "\n"
 
-puts 'My method: ' + range.my_all?.to_s
-puts "Ruby's method: " + range.all?.to_s
+  puts 'My method: ' + range.my_all?.to_s
+  puts "Ruby's method: " + range.all?.to_s
 
-print bar + 'All? - Block given' + bar + "\n"
+  print bar + 'All? - Block given' + bar + "\n"
 
-puts 'My method: ' + (range.my_all? { |n| n.is_a? Integer }).to_s
-puts "Ruby's method: " + (range.all? { |n| n.is_a? Integer }).to_s
+  puts 'My method: ' + (range.my_all? { |n| n.is_a? Integer }).to_s
+  puts "Ruby's method: " + (range.all? { |n| n.is_a? Integer }).to_s
 
-print bar + 'All? - Parameter given' + bar + "\n"
+  print bar + 'All? - Parameter given' + bar + "\n"
 
-puts 'My method: ' + range.my_all?(String).to_s
-puts "Ruby's method: " + range.all?(String).to_s
+  puts 'My method: ' + range.my_all?(String).to_s
+  puts "Ruby's method: " + range.all?(String).to_s
+end
 
 # Test any?
 
-print bar + 'Any? - No arguments' + bar + "\n"
+def check_my_any
+  my_title('Any? - No arguments')
 
-puts 'My method: ' + range.my_any?.to_s
-puts "Ruby's method: " + range.any?.to_s
+  puts 'My method: ' + range.my_any?.to_s
+  puts "Ruby's method: " + range.any?.to_s
 
-print bar + 'Any? - Block given' + bar + "\n"
+  my_title('Any? - Block given')
 
-puts 'My method: ' + (range.my_any? { |n| n.is_a? Integer }).to_s
-puts "Ruby's method: " + (range.any? { |n| n.is_a? Integer }).to_s
+  puts 'My method: ' + (range.my_any? { |n| n.is_a? Integer }).to_s
+  puts "Ruby's method: " + (range.any? { |n| n.is_a? Integer }).to_s
 
-print bar + 'Any? - Parameter given' + bar + "\n"
+  my_title('Any? - Parameter given')
 
-puts 'My method: ' + range.my_any?(String).to_s
-puts "Ruby's method: " + range.any?(String)to_s
+  puts 'My method: ' + range.my_any?(String).to_s
+  puts "Ruby's method: " + range.any?(String).to_s
 
-print bar + 'Any? - Empty Array' + bar + "\n"
+  my_title('Any? - Empty Array')
 
-puts 'My method: ' + empty.my_any?.to_s
-puts "Ruby's method: " + empty.any?.to_s
+  puts "Ruby's method: " + empty.any?.to_s
+  puts 'My method: ' + empty.my_any?.to_s
+end
 
 # Test Inject
-print bar + 'Inject - Range' + bar + "\n"
 
-puts 'My method: ' + range.my_inject(2, :*).to_s
-puts "Ruby's method: " + range.inject(2, :*).to_s
+def check_my_inject
+  print bar + 'Inject - Range' + bar + "\n"
 
-print bar + 'Inject - Block given' + bar + "\n"
+  puts 'My method: ' + range.my_inject(2, :*).to_s
+  puts "Ruby's method: " + range.inject(2, :*).to_s
 
-puts 'My method: ' + (range.my_inject(4) { |n, m| n * m }).to_s
-puts "Ruby's method: " + (range.inject(4) { |n, m| n * m }).to_s
+  print bar + 'Inject - Block given' + bar + "\n"
+
+  puts 'My method: ' + (range.my_inject(4) { |n, m| n * m }).to_s
+  puts "Ruby's method: " + (range.inject(4) { |n, m| n * m }).to_s
+end
